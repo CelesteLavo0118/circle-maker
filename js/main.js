efd825a9da29126afcef1c7c367b899ca3e650b1
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-03-18 09:41:31 -0400

    fixed issues

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $body = $('body');
 
 $('html').on('keydown', function(){
-  var $ball = $('<div>')
+  var $ball = $('<div>');
   var newLeft = Math.random() * 4000;
   var newTop = Math.random() * 1000;
 
